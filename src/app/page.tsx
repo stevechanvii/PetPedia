2bf32365a21540e336f1ee705ed338018219fe8a
stevechanvii <39621091+stevechanvii@users.noreply.github.com>
2024-04-17 22:51:49 +1000

    feat: print pets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,22 @@
 import { useQueryPets } from "@/hooks/query/useQueryPets";
 
 export default function Home() {
-  const { data } = useQueryPets();
+  const { data, isLoading, isError } = useQueryPets();
   console.log(data);
 
-  return <main className="flex">test</main>;
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  return (
+    <main className="flex">
+      {data?.map((pet) => (
+        <div
+          key={pet.name}
+          className="flex flex-col items-center justify-center w-1/3 p-4"
+        >
+          <h1 className="text-2xl font-semibold">{pet.name}</h1>
+        </div>
+      ))}
+    </main>
+  );
 }
